5949fa0950fbd9f8fe0e9de3dabe12e710fec026
AuriRex <xaurirexx@gmail.com>
2024-04-12 00:35:46 +0200

    Remove some debug code

--- GTFO_A2Hash.js
+++ GTFO_A2Hash.js
@@ -26,12 +26,6 @@ function XXHash_PostHash(h32) {
     h32 = Math.imul(h32, 3266489917) >>> 0;
     h32 = (h32 ^ (h32 >>> 16)) >>> 0;
     return h32;
-    // h32 ^= h32 >> 15;
-    // h32 *= 2246822519;
-    // h32 ^= h32 >> 13;
-    // h32 *= 3266489917;
-    // h32 ^= h32 >> 16;
-    // return h32 >>> 0;
 }
 
 function XXHash_Hash(seed, x) {
@@ -49,62 +43,6 @@ function XXHash_Hash_2(seed, x, y) {
     return num;
 }
 
-function XXHash_Hash_dbg(seed, x) {
-    console.log("Hash_dbg: seed: "+seed+", x:"+x);
-    let num = XXHash_PreHash(seed, 4);
-    console.log("PreHash: num: "+num);
-    num = XXHash_StepHash_dbg(num, x);
-    console.log("StepHash: num: "+num);
-    num = XXHash_PostHash_dbg(num);
-    console.log("PostHash: num: "+num);
-    return num;
-}
-
-function XXHash_Hash_dbg_2(seed, x, y) {
-    console.log("Hash_dbg: seed: "+seed+", x:"+x+", y:"+y);
-    let num = XXHash_PreHash(seed, 8);
-    console.log("PreHash: num: "+num);
-    num = XXHash_StepHash_dbg(num, x);
-    console.log("StepHash(x): num: "+num);
-    num = XXHash_StepHash_dbg(num, y);
-    console.log("StepHash(y): num: "+num);
-    num = XXHash_PostHash_dbg(num);
-    console.log("PostHash: num: "+num);
-    return num;
-}
-
-function XXHash_StepHash_dbg(h32, value) {
-    h32 = h32 >>> 0;
-    value = value >>> 0;
-
-    console.log("->StepHash: h32: " + h32 + " value: " + value);
-    let right = Math.imul(value, 3266489917) >>> 0;
-    console.log("->StepHash: right: " + right);
-    let both = (h32 + right) >>> 0;
-    console.log("->StepHash: both: " + both);
-    let rotateLeft = XXHash_RotateLeft(both, 17);
-    console.log("->StepHash: rotateLeft: " + rotateLeft);
-    let result = Math.imul(rotateLeft, 668265263) >>> 0;
-    console.log("->StepHash: result: " + result);
-    return result;
-}
-
-function XXHash_PostHash_dbg(h32) {
-    h32 = h32 >>> 0
-    console.log("->PostHash: h32: " + h32);
-    h32 = (h32 ^ (h32 >>> 15)) >>> 0;
-    console.log("->PostHash: ^h32 >> 15: " + h32);
-    h32 = Math.imul(h32, 2246822519) >>> 0;
-    console.log("->PostHash: h32 *= #1: " + h32);
-    h32 = (h32 ^ (h32 >>> 13)) >>> 0;
-    console.log("->PostHash: ^h32 >> 13: " + h32);
-    h32 = Math.imul(h32, 3266489917) >>> 0;
-    console.log("->PostHash: h32 *= #2: " + h32);
-    h32 = (h32 ^ (h32 >>> 16)) >>> 0;
-    console.log("->PostHash: ^h32 >> 16: " + h32);
-    return h32;
-}
-
 function XXHash_Int(seed, x) {
     let val = XXHash_Hash(seed, x) |0; // Convert to signed int with |0
     return Math.abs(val);
@@ -117,9 +55,7 @@ class HashSequence {
     }
 
     NextInt() {
-        //console.log("NextInt: Seed: "+this.Seed+", SequenceCounter: "+this.SequenceCounter);
         var result = XXHash_Int(this.Seed, this.SequenceCounter);
-        //console.log("NextInt: result: "+result);
         this.SequenceCounter++;
         return result;
     }
@@ -131,14 +67,13 @@ function GenerateComplexPasswordForValues(year, week) {
     let hash = XXHash_Hash_2(0x5e23baf, year, week);
 
     let password = GenerateComplexPassword(hash, 10);
-    //console.log("Year: "+year+", Week: "+week+", Password: "+password);
+
     return password;
 }
 
 function GenerateComplexPasswordBasedOnWeek(length, date) {
     
     if(!date) {
-        //let timestamp = document.getElementById("date").valueAsNumber;
         date = new Date();
     }
 
@@ -146,8 +81,6 @@ function GenerateComplexPasswordBasedOnWeek(length, date) {
     let year = date.getFullYear();
 
     let hash = XXHash_Hash_2(0x5e23baf, year, week);
-
-    //console.log("GenerateComplexPasswordBasedOnWeek: Seed: " + hash);
 
     let password = GenerateComplexPassword(hash, length);
     console.log("Year: "+year+", Week: "+week+", Password: "+password);
@@ -191,71 +124,4 @@ function GenerateComplexPassword(seed, length) {
 Date.prototype.getWeek = function() {
     var onejan = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
-}
-
-function doThing() {
-    //alert(1);
-    //console.log("abcdef");
-
-    // 1169306189
-    // 4294967295
-
-    
-
-    // XXHash_Hash_dbg(0, 1);
-
-    // var pwd = GenerateComplexPassword(0, 10);
-
-    // alert(pwd);
-
-    for(let i = 0; i < 3; i++) {
-        for(let week = 1; week < 53; week++) {
-            let year = 2023 + i;
-            var password = GenerateComplexPasswordForValues(year, week);
-            console.log("Year: "+year+", Week: "+week+", Password: "+password);
-
-        }
-    }
-
-    return;
-
-    // return;
-
-    let password1 = GenerateComplexPassword(0, 10);
-
-    console.log("Password1 (Seed=0): " + password1);
-
-    console.log("###########");
-    console.log("###########");
-    console.log("###########");
-    console.log("###########");
-    console.log("###########");
-
-    let timestamp = document.getElementById("date").valueAsNumber;
-    let date = new Date(timestamp);
-
-    let week = date.getWeek();
-    let year = date.getFullYear();
-
-    let hash = XXHash_Hash_2(0x5e23baf, year, week);
-
-    console.log("GenerateComplexPasswordBasedOnWeek: Seed: " + hash);
-
-    let password0 = GenerateComplexPassword(hash, 10);
-
-    let hashXY = XXHash_Hash_2(0x5e23baf, 2024, 14);
-    console.log("HashXY: " + hashXY);
-
-    console.log(";;;;;;;;;;;;;;;;;;;;;;;;");
-    console.log(";;;;;;;;;;;;;;;;;;;;;;;;");
-    console.log(";;;;;;;;;;;;;;;;;;;;;;;;");
-    console.log(";;;;;;;;;;;;;;;;;;;;;;;;");
-    console.log(";;;;;;;;;;;;;;;;;;;;;;;;");
-    console.log("Hash_DBG_2:");
-
-    var resultXY = XXHash_Hash_dbg_2(0x5e23baf, 2024, 14);
-
-    console.log("resultXY: "+resultXY);
-
-    alert(week + " - " + year + " => " + password);
 }
